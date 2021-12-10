7e04a9c07a9496339aa2d8c256b5cfb3ec9b184e
sitahiro <geed5141@gmail.com>
2021-12-10 14:33:19 +0900

    Revert "Merge pull request #9 from sitahiro/second_training"
    This reverts commit 9243aa3511df27f57e90c2035917e35eebce4833, reversing
    changes made to 72881edb9ac0b00688e598af0517334f5e3bc5e3.

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -15,12 +15,11 @@ class CalendarsController < ApplicationController
   private
 
   def plan_params
-    params.require(:plan).permit(:date, :plan)
+    params.require(:calendars).permit(:date, :plan)
   end
 
   def get_week
     wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
-    @wdays = Date.today.wday
 
     # Dateオブジェクトは、日付を保持しています。下記のように`.today.day`とすると、今日の日付を取得できます。
     @todays_date = Date.today
@@ -35,14 +34,7 @@ class CalendarsController < ApplicationController
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-
-      wday_num = @wdays
-      if @wdays >= 7
-        wday_num = wday_num -7
-      end
-
-      days = { month: (@todays_date + x).month, date: (@todays_date + x).day, plans: today_plans, wday:today_plans }
-
+      days = { month:  (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans}
       @week_days.push(days)
     end
 
